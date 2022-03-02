81aea758c87407ba09f8670e32b407f010dd7d8f
rohit4242 <rohitluni4242@gmail.com>
2022-03-02 18:55:45 +0530

    init

--- Printing Tokens.c
+++ Printing Tokens.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
 
-int main()
-{
-    char *str,i;
+int main() {
 
-    str = (char*)malloc(1024*sizeof(char));
-
-    gets(str);
-
-    for(str=0;str<='\0';i++)
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    
+    for(int i = 0; i < strlen(s); i++)
     {
-        if(str[i]==" ")
+        if(s[i]==' ')
         {
             printf("\n");
         }
         else
         {
-            printf("%s",*(str+i));
+            printf("%c",s[i]);
         }
     }
-
-    printf("\n%s",str);
-
-    free(str);
 
     return 0;
 }
